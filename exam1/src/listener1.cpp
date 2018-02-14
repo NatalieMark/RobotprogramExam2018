7bac6791cddd36240a889171648744fe26c5be54
NatalieMark <natalies2009@hotmail.com>
2018-02-14 22:44:51 +0100

    again

--- exam1/src/listener1.cpp
+++ exam1/src/listener1.cpp
@@ -167,37 +167,35 @@ int main(int argc, char** argv){
 
 	UI_sub = n.subscribe("Commands", 1000, chatterCallback); // (subscribing to, queue space, "update from publisher" - everytime something is published to the topics commands it will be written to the chatterCallback function)
   	pose_subscriber = n.subscribe("/turtle1/pose", 100, poseCallback);
-  	pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1); // advertizing <What is pubished to>(the topic, queue size)
+  	pub = n.advertise.<geometry_msgs::Twist>("/turtle1/cmd_vel", 1); // advertizing <What is pubished to>(the topic, queue size)
   	pub1 = n.advertise<std_msgs::String>("Done", 1); // advertizing <What is pubished to>(the topic, queue size)
 
 	ros::Rate rate(10); // the Rate instance will attempt to keep the loop at 10hz by accounting for the time used by the work done during the loop.
 
 	while(ros::ok()){
 		moving();
-
 		if(answer == "1") {
- 			moving();
- 			circle();
- 			ros::spinOnce();
- 			answer = "empty";
- 		}
- 		else if(answer == "2"){
- 			moving();
- 		  	square();
- 			ros::spinOnce();
- 			answer = "empty";
- 		}
- 		else if(answer == "3"){
- 			moving();
- 			randomwalk();
- 			ros::spinOnce();
- 			answer = "empty";
- 		}
- 		else{
- 			needNew();
+			moving();
+			circle();
+			ros::spinOnce();
+			answer = "hej";
 		}
-
-		ros::spinOnce();			
+		else if(answer == "2"){
+			moving();
+		  	square();
+			ros::spinOnce();
+			answer = "hej";
+		}
+		else if(answer == "3"){
+			moving();
+			randomwalk();
+			ros::spinOnce();
+			answer = "hej";
+		}
+		else
+			needNew();
+			
+		ros::spinOnce();
 	}
 	rate.sleep();
 	return 0;
